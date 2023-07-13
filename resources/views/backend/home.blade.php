b57bbc1f79c3935a3c37ecd5f5f33287595063ae
Y2theK <yeyintkyaw.dev@gmail.com>
2023-07-14 03:42:00 +0630

    Windmill Dashboard Setup

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -1,8 +1,1 @@
-<x-admin-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Home') }}
-        </h2>
-    </x-slot>
-
-</x-admin-layout>
+@extends('backend.layouts.app')
