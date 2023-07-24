64b30e5ff475567f28cfd8e640286c66fcf2e2cc
Y2theK <yeyintkyaw.dev@gmail.com>
2023-07-25 04:55:45 +0630

    Wallet FirstorCreate in Admin User Update

--- app/Http/Controllers/backend/UserController.php
+++ app/Http/Controllers/backend/UserController.php
@@ -26,41 +26,42 @@ class UserController extends Controller
     {
         return view('backend.user.index');
     }
-    
-    public function getUsers(){
+
+    public function getUsers()
+    {
         $data = User::query();
         return DataTables::of($data)
-        ->editColumn('created_at', function ($row) {
-            return 'Since ' .$row->created_at->format('Y, M d'); // Customize the date format as needed
-        })
-        ->editColumn('updated_at', function ($row) {
-            return $row->updated_at->format('Y, M d h:m:s a'); // Customize the date format as needed
-        })
-        ->editColumn('user_agent',function($row){
-            if($row->user_agent){
-                $agent = new Agent();
-                $agent->setUserAgent($row->user_agent);
-                $device = $agent->device();
-                $platform = $agent->platform();
-                $browser = $agent->browser();
+            ->editColumn('created_at', function ($row) {
+                return 'Since ' . $row->created_at->format('Y, M d'); // Customize the date format as needed
+            })
+            ->editColumn('updated_at', function ($row) {
+                return $row->updated_at->format('Y, M d h:m:s a'); // Customize the date format as needed
+            })
+            ->editColumn('user_agent', function ($row) {
+                if ($row->user_agent) {
+                    $agent = new Agent();
+                    $agent->setUserAgent($row->user_agent);
+                    $device = $agent->device();
+                    $platform = $agent->platform();
+                    $browser = $agent->browser();
 
-                $htmlTable = '<table class="">
-                <tr ><th>Device</th><td class="text-yellow-500">'.$device.'</td></tr>
-                <tr><th>Platform</th><td class="text-green-500">'.$platform.'</td></tr>
-                <tr><th>Browser</th><td class="text-red-500">'.$browser.'</td></tr>
+                    $htmlTable = '<table class="">
+                <tr ><th>Device</th><td class="text-yellow-500">' . $device . '</td></tr>
+                <tr><th>Platform</th><td class="text-green-500">' . $platform . '</td></tr>
+                <tr><th>Browser</th><td class="text-red-500">' . $browser . '</td></tr>
                 </table>';
 
-                return $htmlTable;
-            }
-        })
-        ->addColumn('action', function ($row) {
-            $edit_icon = '<a href="'.route('admin.users.edit',$row->id).'" class="text-yellow-500 mr-3 text-xl hover:text-yellow-300"> <i class="las la-edit"></i></a>';
-            $delete_icon = '<a href="#" class="text-red-500 text-xl hover:text-red-300 confirm-delete" data-id="'.$row->id.'"> <i class="las la-trash"></i></a>';
+                    return $htmlTable;
+                }
+            })
+            ->addColumn('action', function ($row) {
+                $edit_icon = '<a href="' . route('admin.users.edit', $row->id) . '" class="text-yellow-500 mr-3 text-xl hover:text-yellow-300"> <i class="las la-edit"></i></a>';
+                $delete_icon = '<a href="#" class="text-red-500 text-xl hover:text-red-300 confirm-delete" data-id="' . $row->id . '"> <i class="las la-trash"></i></a>';
 
-            return ' <div class="action-icons">'. $edit_icon. $delete_icon.'</div> ';
-        })
-        ->rawColumns(['user_agent','action'])
-        ->make(true);
+                return ' <div class="action-icons">' . $edit_icon . $delete_icon . '</div> ';
+            })
+            ->rawColumns(['user_agent', 'action'])
+            ->make(true);
     }
 
     /**
@@ -89,7 +90,7 @@ class UserController extends Controller
             $user->phone = $request->phone;
             $user->password = Hash::make($request->password);
             $user->save();
-    
+
             Wallet::firstOrCreate(
                 [
                     'user_id' => $user->id
@@ -101,15 +102,11 @@ class UserController extends Controller
             );
 
             DB::commit();
-            return redirect()->route('admin.users.index')->with('created','Created Successfully');
-
+            return redirect()->route('admin.users.index')->with('created', 'Created Successfully');
         } catch (\Exception $e) {
             DB::rollBack();
             return back()->withErrors(['failed' => 'Something Wrong'])->withInput();
-
         }
-        
-
     }
 
     /**
@@ -132,7 +129,7 @@ class UserController extends Controller
     public function edit($id)
     {
         $user = User::findOrFail($id);
-        return view('backend.user.edit',compact('user'));
+        return view('backend.user.edit', compact('user'));
     }
 
     /**
@@ -144,14 +141,31 @@ class UserController extends Controller
      */
     public function update(UserUpdateRequest $request, $id)
     {
-        $user = User::findOrFail($id);
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->phone = $request->phone;
-        $user->password = $request->password ? Hash::make($request->password) : $user->password;
-        $user->save();
+        DB::beginTransaction();
+        try {
+            $user = User::findOrFail($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->phone = $request->phone;
+            $user->password = $request->password ? Hash::make($request->password) : $user->password;
+            $user->save();
 
-        return redirect()->route('admin.users.index')->with('updated','Successfully Updated');
+            Wallet::firstOrCreate(
+                [
+                    'user_id' => $user->id
+                ],
+                [
+                    'account_number' => UUIDGenerater::accountNumber(),
+                    'amount' => 0
+                ]
+            );
+
+            DB::commit();
+            return redirect()->route('admin.users.index')->with('updated', 'Successfully Updated');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return back()->withErrors(['failed' => 'Something Wrong'])->withInput();
+        }
     }
 
     /**
