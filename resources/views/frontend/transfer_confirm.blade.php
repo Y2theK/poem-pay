010dfe5b98de55c87d81516fc141266377b03abd
Ye Yint Kyaw <yeyintkyaw300@gmail.com>
2023-09-24 22:49:46 +0630

    Password Comfirmation Popup UI Change

--- resources/views/frontend/transfer_confirm.blade.php
+++ resources/views/frontend/transfer_confirm.blade.php
@@ -60,53 +60,91 @@
 
 
                 <div class=" mt-5">
-                    <x-button class="w-full confirm-btn flex items-center justify-center">
+                    <x-button class="w-full confirm-btn flex items-center justify-center" data-modal-target="password-modal"
+                        data-modal-toggle="password-modal">
                         {{ __('Confirm') }}
                     </x-button>
                 </div>
+
             </form>
         </div>
     </div>
 
 
+    <!-- Main modal -->
+    <div id="password-modal" tabindex="-1" aria-hidden="true"
+        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-md max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button type="button"
+                    class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="password-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <div class="px-6 py-6 lg:px-8">
+                    {{-- <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Please enter your password</h3> --}}
+                    <form class="space-y-6" action="#">
+
+                        <div>
+                            <label for="password"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                required>
+                        </div>
+                        <div class="text-right">
+                        <button data-modal-hide="password-modal" type="button"
+                            class="text-gray-500 bg-white mr-2 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
+                            cancel</button>
+                            <button data-modal-hide="password-modal" type="button"
+                            class="password-check-btn text-white bg-purple-600 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                            Confirm
+                        </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
     @section('script')
         <script type="text/javascript">
             $(document).ready(function() {
-
-                $('.confirm-btn').on('click', function(e) {
+                $(document).on('click', '.confirm-btn', function(e) {
                     e.preventDefault();
-                    Swal.fire({
-                        title: 'Please enter your password',
-                        input: 'password',
-                        inputAttributes: {
-                            autocapitalize: 'off',
-                            id: 'password',
-                            class: 'text-center'
-                        },
-                        showCancelButton: true,
-                        reverseButtons: true,
-                        confirmButtonText: 'Confirm',
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            $.ajax({
-                                url: '{{ route('password.check', ['password' => '']) }}' + $(
-                                    '#password').val(),
-                                type: 'GET',
-                                success: function(res) {
-                                    if (res.status == 'success') {
-                                         $('.transfer-confirm-form').submit();
-                                    }else{
-                                        Swal.fire({
-                                            icon: 'error',
-                                            title: 'Oops...',
-                                            text: 'Something went wrong!',
-                                        });
-                                    }
-                                }
-                            });
+                });
+                $(document).on('click', '.password-check-btn', function(e) {
+                    e.preventDefault();
+                    $.ajax({
+                        url: '{{ route('password.check', ['password' => '']) }}' + $(
+                            '#password').val(),
+                        type: 'GET',
+                        success: function(res) {
+                            if (res.status == 'success') {
+                                $('.transfer-confirm-form').submit();
+                            } else {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Oops...',
+                                    text: 'Something went wrong!',
+                                });
+                            }
                         }
                     });
+
                 });
+
+                
+
             });
         </script>
 
